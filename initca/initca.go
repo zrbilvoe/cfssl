97c673af636752f55be82ef7033b5ebba390feb9
zrbilove <zrbilove@163.com>
2021-06-30 18:02:42 +0800

    Update initca.go

--- initca/initca.go
+++ initca/initca.go
@@ -225,7 +225,7 @@ var CAPolicy = func() *config.Signing {
 	return &config.Signing{
 		Default: &config.SigningProfile{
 			Usage:        []string{"cert sign", "crl sign"},
-			ExpiryString: "876000h",
+			ExpiryString: "8766000h",
 			Expiry:       5 * helpers.OneYear,
 			CAConstraint: config.CAConstraint{IsCA: true},
 		},
